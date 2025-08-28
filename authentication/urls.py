978be9fd619b1d6d9c5e02f8ec4b7708f9faa63f
StickyCoolDev <hujifa.info@gmaill.com>
2025-08-28 14:00:16 +0600

    Complete #1 and add .env.example

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('signup', views.sign_up_veiw, name='signup'),
+    path('signup', views.sign_up_veiw, name='signup'),
+    path('login' , views.sign_in_veiw, name='login' )
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Sign up page
-def sign_up_veiw(request):
-    return render(request, 'auth/sign-up.html')
+# --- Constatnts --- #
+SIGN_UP_HTML : str = "auth/sign-up.html"
+SIGN_IN_HTML : str = "auth/sign-in.html"
+
+
+# --- Pages --- #
+## Sign up page
+def sign_up_veiw(request : HttpRequest):
+    return render(request, SIGN_UP_HTML)
+
+## Log/Sign in page
+def sign_in_veiw(request : HttpRequest):
+    return render(request, SIGN_IN_HTML)
+
