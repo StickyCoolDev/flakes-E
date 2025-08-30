a2b2c65dc271fef6c551a1d0b3816a91920cba22
StickyCoolDev <hujifa.info@gmaill.com>
2025-08-30 20:52:02 +0600

    Cleanup and close #2

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.auth_veiw, name='Auth'),
     path('signup', views.sign_up_veiw, name='signup'),
     path('login' , views.sign_in_veiw, name='login' )
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -4,9 +4,15 @@ from django.http import HttpRequest
 # --- Constatnts --- #
 SIGN_UP_HTML : str = "auth/sign-up.html"
 SIGN_IN_HTML : str = "auth/sign-in.html"
+BASE_AUTH_HTML : str = "auth/auth.html"
 
 
 # --- Pages --- #
+## Base Auth page
+def auth_veiw(request : HttpRequest):
+    return render(request, BASE_AUTH_HTML)
+
+
 ## Sign up page
 def sign_up_veiw(request : HttpRequest):
     return render(request, SIGN_UP_HTML)
